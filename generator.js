82c31a5faedc0cf17491a109d1bf1016f888fb5c
david <david.ostler001@gmail.com>
2017-12-07 11:55:28 -0700

    Adds js create/remove education elements

--- generator.js
+++ generator.js
@@ -1,1 +1,29 @@
-alert("hi")
+function addEducationItem(){
+	let educationList = document.getElementById("educationList")
+	let newItem = document.createElement("li")
+	newItem.innerHTML = '<div>\
+                            <div class="left">\
+                                <button class="removeItem" onclick="removeEducationItem(this)">X</button>\
+                            </div>\
+                            <div class="right">\
+                                <select class="form-control degreeType">\
+                                    <option value="bachelors">Bachelors</option>\
+                                    <option value="masters">Masters</option>\
+                                    <option value="phd">Ph.D.</option>\
+                                </select>\
+                                <input type="text" class="form-control schoolName" placeholder="School Name">\
+                                <input type="text" class="form-control degreeName" placeholder="Degree Name">\
+                            </div>\
+                        </div>'
+
+	educationList.appendChild(newItem)
+	console.log(educationList)
+}
+
+function removeEducationItem(element){
+	console.log(element)
+	let educationList = document.getElementById("educationList")
+	let listItem = element.parentNode.parentNode.parentNode
+
+	educationList.removeChild(listItem)
+}
