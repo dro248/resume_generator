91deeb4ab9b906ea32382243bcb61a45a5444462
david <david.ostler001@gmail.com>
2017-12-07 12:41:25 -0700

    Adds experience functionality (js)

--- generator.js
+++ generator.js
@@ -1,3 +1,6 @@
+
+// EDUCATION
+
 function addEducationItem(){
 	let educationList = document.getElementById("educationList")
 	let newItem = document.createElement("li")
@@ -17,13 +20,41 @@ function addEducationItem(){
                         </div>'
 
 	educationList.appendChild(newItem)
-	console.log(educationList)
 }
 
-function removeEducationItem(element){
-	console.log(element)
+function removeEducationItem(education_element){
 	let educationList = document.getElementById("educationList")
-	let listItem = element.parentNode.parentNode.parentNode
+	let listItem = education_element.parentNode.parentNode.parentNode
 
 	educationList.removeChild(listItem)
+}
+
+
+
+// EXPERIENCE
+
+function addExperienceItem(){
+	let educationList = document.getElementById("experienceList")
+	let newItem = document.createElement("li")
+	newItem.innerHTML = '<div class="experienceItem">\
+                            <div class="left">\
+                                <button class="removeItem" onclick="removeExperienceItem(this)">X</button>\
+                            </div>\
+                            <div class="right">\
+                                <input type="text" class="form-control jobTitle" placeholder="Job Title">\
+                                <input type="text" class="form-control companyName" placeholder="Company Name"><br>\
+                                <label for="startDate">Start Date:&nbsp;</label><input type="date" name="startDate" class="form-control date" placeholder="Start Date">\
+                                <label for="endDate">End Date:&nbsp;&nbsp;</label><input type="date" name="endDate" class="form-control date" placeholder="End Date">\
+                                <textarea type="text" class="form-control jobDescription" placeholder="Job Description"></textarea>\
+                            </div>\
+                        </div>'
+
+    experienceList.appendChild(newItem)
+}
+
+function removeExperienceItem(experience_element){
+	let experienceList = document.getElementById("experienceList")
+	let listItem = experience_element.parentNode.parentNode.parentNode
+
+	experienceList.removeChild(listItem)
 }
